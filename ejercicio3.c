fbd8613c73a1c2082ba3185d37e5ea5b2d5c67d5
artekcl9-glitch <artek.cl9@gmail.com>
2026-09-14 16:21:23 -0300

    5

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int x,f,y;
+    int x;
+    float c,f;
 
     printf("Indique la temperatura usada para la convercion:\n");
     printf("\n1.Fahrenheit\n2.Celcius\n");
@@ -9,8 +10,16 @@ int main(){
 
     switch(x){
         case 1: printf("Indique porfavor la temperatura: ");
-        scanf("%i",&f);break;
+        scanf("%f",&f);
+        c=(f-32)*5/9;
+        printf("La temperatura en Celcius es de: %f\n",c);break;
         case 2: printf("Indique porfavor la temperatura: ");
-        scanf("%i",&c);break;
-    }switch
+        scanf("%f",&c);
+        f=(c*9/5)+32;
+        printf("La temperatura en Fahrenheir es de: %f\n",f);break;
+        default: printf("Hola, lo que pusiste esta mal\n");
+    }
+
+    getchar();
+    return 0;
 }
